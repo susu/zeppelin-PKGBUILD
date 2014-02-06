937956a0810bb1b27b33040f3821b27f52b6dbed
Marton Suranyi <marton.suranyi@gmail.com>
2014-02-06 23:42:53 +0100

    trivial errors in upload-aur.sh fixed

--- upload-aur.sh
+++ upload-aur.sh
@@ -8,9 +8,11 @@ error() # msg
   exit 42
 }
 
-for pkg in zeppelin-*-git; do
-  echo "Creating source package: ${pkg}"
+for pkg in zeppelin*git; do
+  echo "Cleaning up previous src packages..."
   cd ${base}/${pkg} || error "ERROR"
+  rm -vf ${pkg}*.src.tar.gz
+  echo "Creating source package: ${pkg}"
   makepkg -Sf || error "ERROR"
 
   burp -c multimedia -u asusu ${pkg}*.src.tar.gz || error "ERROR"
